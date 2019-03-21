205dc4ab446c0e6e2d35f27a5c9b5275697ffb65
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-03-21 00:10:44 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,6 +8,6 @@ class GenresController < ApplicationController
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params["slug"])
-    erb :"/artists/show"
+    erb :"/genres/show"
   end
 end
